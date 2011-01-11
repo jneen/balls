a90ff82c7e897af3812f098424c4e9a975abd525
Jay Adkisson <jjmadkisson@gmail.com>
2011-01-11 15:19:26 -0800

    implement <%= %> and <%- %>
    also factor out some things into util.sh

--- lib/util.sh
+++ lib/util.sh
@@ -32,8 +32,63 @@ pluralize() {
   fi
 }
 
+#trim() {
+#  trimmed="$1"
+#  trimmed="${trimmed##*( )}"
+#  trimmed="${trimmed%%*( )}"
+#}
+
+read_char() {
+  IFS= read -d"$(echo -e '\004')" -n1 "$@"
+}
+
+read_until() {
+  local glob=$1; shift
+  local var=$1; shift
+
+  local out
+
+  while read_char ch; do
+    out="${out}${ch}"
+    case "$out" in
+      *$glob)
+        FOUND=1
+        export $var="${out%$glob}"
+        return 0
+      ;;
+    esac
+  done
+
+  # we've hit an EOF
+  if [ -z "$out" ]; then
+    false
+  else
+    FOUND=0
+    export $var="$out"
+    true
+  fi
+}
+
+trim_l() {
+  local str=${!1}
+  str="${str##+([[:space:]])}"
+  export "$1"="$str"
+}
+
+trim_r() {
+  local str=${!1}
+  str="${str%%+([[:space:]])}"
+  export "$1"="$str"
+}
+
 trim() {
-  trimmed="$1"
-  trimmed="${trimmed##*( )}"
-  trimmed="${trimmed%%*( )}"
+  trim_l "$@"
+  trim_r "$@"
+}
+
+# escape ' with '\''.  sorry everyone.
+bash_safe() {
+  local str="${!1}."
+  str="$(echo "$str" | sed "s/'/'\\\\''/g")"
+  export "$1"="'${str:0:${#str}-1}'"
 }
