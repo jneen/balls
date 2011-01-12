70ce9fd4b5573d4517bb9b95b1fae2c9565f3d9a
Jay Adkisson <jjmadkisson@gmail.com>
2011-01-11 19:03:32 -0800

    options options options

--- lib/balls.sh
+++ lib/balls.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
+[[ -z "$BALLS_LIB" ]] && BALLS_LIB=.
+
+[[ -z "$BALLS_CONF" ]] && BALLS_CONF=./config.sh
+[[ -z "$BALLS_ROOT" ]] && BALLS_ROOT=$(readlink -f "$(dirname $BALLS_CONF)/../")
 [[ -z "$BALLS_LIB" ]] && BALLS_LIB=$(dirname $0)
 
-[[ -z "$TMP_DIR" ]] && TMP_DIR=/tmp/balls
-[[ -d "$TMP_DIR" ]] && mkdir "$TMP_DIR"
+[[ -z "$BALLS_TMP" ]] && BALLS_TMP=/tmp/balls
+[[ -d "$BALLS_TMP" ]] || mkdir "$BALLS_TMP"
 
 [[ -z "$BALLS_PORT" ]] && BALLS_PORT=3000
+
+[[ -z "$BALLS_VIEWS" ]] && BALLS_VIEWS=$BALLS_ROOT/views
+[[ -z "$BALLS_ACTIONS" ]] && BALLS_ACTIONS=$BALLS_ROOT/actions
 
 
 . $BALLS_LIB/util.sh
 . $BALLS_LIB/http.sh
 . $BALLS_LIB/router.sh
 . $BALLS_LIB/server.sh
+. $BALLS_ACTIONS/*.sh
