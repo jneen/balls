016194f754ed27893f1bb0a6676150b275b0b64f
Jay Adkisson <jjmadkisson@gmail.com>
2011-01-11 21:22:43 -0800

    compile the views on an intelligent basis

--- lib/view.sh
+++ lib/view.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 esh=$(readlink -f $BALLS_LIB/../bin/esh)
 render::esh() {
-  local src="$($esh $BALLS_VIEWS/$1)"
-  (
-    eval "$src"
-  )
+  local view="$BALLS_VIEWS/$1"
+  local compiled="$(esh::compile "$view")"
+
+  # source it in a subshell so it gets our variables
+  ( . "$compiled" )
+}
+
+esh::compile() {
+  local view="$1"
+  local compiled_fname="$view.o"
+
+  if [[ ! -f "$compiled_fname" ]] || [[ "$view" -nt "$compiled_fname" ]]; then
+    $esh "$view" > "$compiled_fname"
+  fi
+
+  echo "$compiled_fname"
 }
